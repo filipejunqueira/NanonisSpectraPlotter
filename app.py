ff620bd8e8304def9224c1d7b03f7a49a5c599c5
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2022-01-20 20:00:00 +0000

    Prettying stuff up

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import dash
 import dash_bootstrap_components as dbc
 import numpy as np
 from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash_bootstrap_templates import load_figure_template
 from plotly import graph_objects as go
 
@@ -21,13 +21,12 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY, dbc_css])
 app.title = "Spectra Explorer"
 load_figure_template(["darkly"])
 
-top_fig = go.Figure()
-top_fig.update_layout(title="Spectra Position",
-                      width=600,
-                      height=600,
-                      autosize=True,
-                      template="darkly",
-                      margin={'t': 100, 'b': 20, 'r': 20, 'l': 20})
+image_fig = go.Figure()
+image_fig.update_layout(title="Spectra Position",
+                        width=600,
+                        height=600,
+                        autosize=True,
+                        margin={'t': 100, 'b': 20, 'r': 20, 'l': 20})
 
 spectra_fig = go.Figure()
 spectra_fig.update_layout(title="Spectra",
@@ -39,12 +38,13 @@ spectra_fig.update_layout(title="Spectra",
 @app.callback(Output('ref-image', 'figure'),
               Output('spectra-data', 'data'),
               Output('image-dropdown', 'options'),
+              Output('y-channel-dropdown', 'options'),
               Input("spectra-upload", 'value'),
               Input('sxm-upload', 'value'),
               Input('image-dropdown', 'value'))
 def set_core_figs(spectra_path, sxm_path, image_channel):
     if spectra_path is (None or ""):
-        return top_fig, json.dumps(""), []
+        return image_fig, json.dumps(""), [], []
 
     dot3ds_data = load_grid(spectra_path)
     dot3ds_data_dict = dot3ds_2dict(dot3ds_data)
@@ -56,9 +56,10 @@ def set_core_figs(spectra_path, sxm_path, image_channel):
         sxm_data = None
 
     dropdown_opts = build_dropdown_options(dot3ds_data, sxm_data)
+    channel_dropdown_opts = [{"label": val, "value": val} for val in dot3ds_data_dict["channels"]]
 
     if image_channel is None:
-        return go.Figure(), json.dumps(dot3ds_data_dict), dropdown_opts
+        return go.Figure(), json.dumps(dot3ds_data_dict), dropdown_opts, channel_dropdown_opts
     elif image_channel in dot3ds_data_dict.keys():
         res = dot3ds_data.header["dim_px"][::-1]
         resizing = res + [-1]
@@ -70,50 +71,72 @@ def set_core_figs(spectra_path, sxm_path, image_channel):
     # updated_top_fig = callbacks.set_title(updated_top_fig, dot3ds_data.basename)
     updated_top_fig = callbacks.plot_positions_vs_image(dot3ds_data_dict, background_img)
 
-    return updated_top_fig, json.dumps(dot3ds_data_dict), dropdown_opts
+    return updated_top_fig, json.dumps(dot3ds_data_dict), dropdown_opts, channel_dropdown_opts
 
 
 @app.callback(Output('ref-spectra', 'figure'),
               Input('ref-image', 'clickData'),
-              Input('ref-image', 'selectedData'))
-def spectraplotter(clickdata, selectdata):
+              Input('ref-image', 'selectedData'),
+              State('spectra-data', 'data'),
+              Input('y-channel-dropdown', 'value'))
+def spectraplotter(clickdata, selectdata, dot3dsdata_dict, selected_y_channels):
+    if dot3dsdata_dict is None:
+        return spectra_fig
+
+    dot3dsdata_dict = json.loads(dot3dsdata_dict)
     useful_data = combine_click_selects([clickdata, selectdata])
 
+    for pointindex, metadata in useful_data.items():
+        for y_channel in selected_y_channels:
+            print(y_channel)
+            print(dot3dsdata_dict[y_channel])
+            print(useful_data[pointindex])
+            spectra_fig.add_trace(go.Scatter(x=dot3dsdata_dict["sweep_signal"],
+                                             y=dot3dsdata_dict[y_channel][useful_data[pointindex]]))
 
-    return go.Figure()
+    return spectra_fig
 
 
 root_layout = html.Div([
     html.Hr(),
     html.Div([
         dcc.Markdown("** Spectra: **",
-                     style={'width': '150px', 'display': 'inline-block'}),
+                     style={'width': '150px',
+                            'margin': {'l': '10px'},
+                            'display': 'inline-block'}),
         dcc.Input(
             id="spectra-upload",
             type="text",
             persistence=True,
             debounce=True,
-            style={'width': '450px'},
+            style={'width': '1800px'},
             placeholder=".3ds or .dat")]),
     html.Div([
         dcc.Markdown("** Image (opt.): **",
-                     style={'width': '150px', 'display': 'inline-block'}),
+                     style={'width': '150px',
+                            'margin': {'l': '10px'},
+                            'display': 'inline-block'}),
         dcc.Input(
             id="sxm-upload",
             type="text",
             persistence=True,
             debounce=True,
-            style={'width': '450px'},
+            style={'width': '1800px'},
             placeholder=".sxm")]),
     html.Hr(),
-    html.Div(
+    html.Div([
         dcc.Dropdown(id="image-dropdown",
-                     style={'width': '600px'})),
+                     style={'width': '600px',
+                            'display': 'inline-block'}),
+        dcc.Dropdown(id="y-channel-dropdown",
+                     multi=True,
+                     style={'width': '1200px',
+                            'display': 'inline-block'})]),
     html.Hr(),
     html.Div([
         dcc.Graph(
             id='ref-image',
-            figure=top_fig,
+            figure=image_fig,
             style={'display': 'inline-block'}),
         dcc.Graph(
             id='ref-spectra',
--- callbacks.py
+++ callbacks.py
@@ -6,19 +6,6 @@ from data import dot3ds_params2pd
 from utils import build_spectra_hover, mpl_to_plotly
 
 
-def set_title(top_fig, filename):
-    if filename is None:
-        top_fig.update_layout(title="Waiting...")
-    elif ".3ds" in filename:
-        top_fig.update_layout(title=filename)
-    elif ".dat" in filename:
-        top_fig.update_layout(title=filename)
-    else:
-        top_fig.update_layout(title="Unsuppprted File")
-
-    return top_fig
-
-
 def plot_positions_vs_image(dot3ds_data_dict, img):
     dot3ds_pandas = dot3ds_params2pd(dot3ds_data_dict)
 
@@ -33,19 +20,18 @@ def plot_positions_vs_image(dot3ds_data_dict, img):
                          dot3ds_data_dict["dim_px"][1])
 
     # Plotting
-    top_fig = px.imshow(img, color_continuous_scale=mpl_to_plotly(nanomap), x=x_axis, y=y_axis,
-                        origin="lower", aspect="equal")
-    top_fig.add_trace(go.Scatter(x=dot3ds_pandas["X (m)"], y=dot3ds_pandas["Y (m)"], mode="markers",
-                                 hoverinfo='text',
-                                 text=dot3ds_pandas.columns,
-                                 customdata=dot3ds_pandas.values,
-                                 hovertemplate=build_spectra_hover(dot3ds_pandas)))
+    spectra_fig = px.imshow(img, color_continuous_scale=mpl_to_plotly(nanomap), x=x_axis, y=y_axis,
+                            origin="lower", aspect="equal")
+    spectra_fig.add_trace(go.Scatter(x=dot3ds_pandas["X (m)"], y=dot3ds_pandas["Y (m)"], mode="markers",
+                                     hoverinfo='text',
+                                     text=dot3ds_pandas.columns,
+                                     customdata=dot3ds_pandas.values,
+                                     hovertemplate=build_spectra_hover(dot3ds_pandas)))
 
-    top_fig.update_layout(title="Spectra Position",
-                          width=600,
-                          height=600,
-                          autosize=True,
-                          template="darkly",
-                          margin={'t': 100, 'b': 20, 'r': 20, 'l': 20})
+    spectra_fig.update_layout(title="Spectra Position",
+                              width=600,
+                              height=600,
+                              autosize=True,
+                              margin={'t': 100, 'b': 20, 'r': 20, 'l': 20})
 
-    return top_fig
+    return spectra_fig
