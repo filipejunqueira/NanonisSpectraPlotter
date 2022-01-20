Fix incorrect rotation

app.py
@@ -61,7 +61,7 @@ def set_core_figs(spectra_path, sxm_path, image_channel):
     if image_channel is None:
         return go.Figure(), json.dumps(dot3ds_data_dict), dropdown_opts
     elif image_channel in dot3ds_data_dict.keys():
-        res = dot3ds_data.header["dim_px"]
+        res = dot3ds_data.header["dim_px"][::-1]
         resizing = res + [-1]
         background_img = np.array(dot3ds_data_dict[image_channel]).reshape(resizing)[:, :,
                          0]  # for now slice it, replace with slider in future

callbacks.py
@@ -22,14 +22,18 @@ def set_title(top_fig, filename):
 def plot_positions_vs_image(dot3ds_data_dict, img):
     dot3ds_pandas = dot3ds_params2pd(dot3ds_data_dict)
 
-    centre = dot3ds_data_dict["pos_xy"]
-    size = dot3ds_data_dict["size_xy"]
+    x_square = np.abs((dot3ds_pandas["X (m)"].max() - dot3ds_pandas["X (m)"].min()) / (dot3ds_data_dict["dim_px"][0] - 1))
+    y_square = np.abs((dot3ds_pandas["Y (m)"].max() - dot3ds_pandas["Y (m)"].min()) / (dot3ds_data_dict["dim_px"][1] - 1))
 
-    x_axis = np.linspace(centre[0] - size[0] / 2, centre[0] + size[0] / 2, img.shape[0])
-    y_axis = np.linspace(centre[1] - size[1] / 2, centre[1] + size[1] / 2, img.shape[1])
+    x_axis = np.linspace(dot3ds_pandas["X (m)"].min() - (x_square / 2),
+                         dot3ds_pandas["X (m)"].max() + (x_square / 2),
+                         dot3ds_data_dict["dim_px"][0])
+    y_axis = np.linspace(dot3ds_pandas["Y (m)"].min() - (y_square / 2),
+                         dot3ds_pandas["Y (m)"].max() + (y_square / 2),
+                         dot3ds_data_dict["dim_px"][1])
 
     # Plotting
-    top_fig = px.imshow(np.rot90(img), color_continuous_scale=mpl_to_plotly(nanomap), x=x_axis, y=y_axis,
+    top_fig = px.imshow(img, color_continuous_scale=mpl_to_plotly(nanomap), x=x_axis, y=y_axis,
                         origin="lower", aspect="equal")
     top_fig.add_trace(go.Scatter(x=dot3ds_pandas["X (m)"], y=dot3ds_pandas["Y (m)"], mode="markers",
                                  hoverinfo='text',

utils.py
@@ -10,7 +10,7 @@ def mask_nan(img: np.ndarray, nan_value=0):
     return img
 
 
-def mpl_to_plotly(cmap, pl_entries=11, rdigits=2):
+def mpl_to_plotly(cmap, pl_entries=30, rdigits=6):
     scale = np.linspace(0, 1, pl_entries)
     colors = (cmap(scale)[:, :3]*255).astype(np.uint8)
     pl_colorscale = [[round(s, rdigits), f'rgb{tuple(color)}'] for s, color in zip(scale, colors)]